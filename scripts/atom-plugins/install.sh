10549b1bd29543115d736d4637c611573cc2dd0a
Michael Scherer <mail@michael-scherer.net>
2015-10-24 08:00:14 +0200

    Add more Atom plugins.

--- scripts/atom-plugins/install.sh
+++ scripts/atom-plugins/install.sh
@@ -1,10 +1,16 @@
 apm Zen
 apm atom-pair
 apm atom-ternjs
-apm circle-ci
+apm cucumber
+apm editorconfig
 apm git-log
+apm language-batch
+apm language-docker
 apm language-gherkin
+apm language-powershell
+apm merge-conflicts
 apm minimap
+apm minimap-bookmarks
 apm minimap-codeglance
 apm minimap-find-and-replace
 apm minimap-git-diff
