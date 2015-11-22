e0a13c7cd8a6e80071babdcdf441fe49cf13f940
Michael Scherer <mail@michael-scherer.net>
2015-11-22 16:26:36 +0100

    Allow unattended installation of brew.

--- scripts/10_brew/install.sh
+++ scripts/10_brew/install.sh
@@ -9,7 +9,7 @@ fi;
 if [ "$OS" != "osx" ]; then exit; fi
 
 # Install Brew itself
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
 
 # Install any package given in `packagees.list` (one per line). See: http://stackoverflow.com/questions/10929453/bash-scripting-read-file-line-by-line
 if [ -f "packages.list" ]; then
