f513434d55c91b1c68393bac7c93adb62c7cd4a4
Michael Scherer <mail@michael-scherer.net>
2015-11-22 16:56:35 +0100

    Additional language settings.

--- scripts/language.sh
+++ scripts/language.sh
@@ -9,14 +9,11 @@ fi;
 if [ "$OS" != "osx" ]; then exit; fi
 
 # Set language and text formats
-# Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
-# `Inches`, `en_GB` with `en_US`, and `true` with `false`.
-#defaults write NSGlobalDomain AppleLanguages -array "de"
-#defaults write NSGlobalDomain AppleLocale -string "de_DE@currency=EUR"
-#defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
-#defaults write NSGlobalDomain AppleMetricUnits -bool true
-
 sudo languagesetup -langspec de
+defaults write NSGlobalDomain AppleLanguages -array "de"
+defaults write NSGlobalDomain AppleLocale -string "de_DE@currency=EUR"
+defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+defaults write NSGlobalDomain AppleMetricUnits -bool true
 
 # Set the timezone; see `sudo systemsetup -listtimezones` for other values
 sudo systemsetup -settimezone "Europe/Berlin" > /dev/null
