4fdb517b7f887479824df6b6ac4524a8da2d3d18
Michael Scherer <mail@michael-scherer.net>
2015-11-23 18:29:59 +0100

    Fix command call.

--- init/scale-hdpi/enable.sh
+++ init/scale-hdpi/enable.sh
@@ -12,5 +12,5 @@ if [ "$ISHOST" = true ]; then exit; fi
 cp -rf scale-hdpi.app ~/Desktop
 
 # Allow execution
-sudo tccutil.py --insert com.apple.ScriptEditor.id.scale-hdpi
-sudo tccutil.py --enable com.apple.ScriptEditor.id.scale-hdpi
+sudo ./tccutil.py --insert com.apple.ScriptEditor.id.scale-hdpi
+sudo ./tccutil.py --enable com.apple.ScriptEditor.id.scale-hdpi
