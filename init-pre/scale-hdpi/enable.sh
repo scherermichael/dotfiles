bb72f2b58261a4a3807ab11683ddea107cc459ab
Michael Scherer <michael.scherer@sealsystems.de>
2015-11-23 17:36:02 +0100

    Make script to copy scale-hdpi.app if it's a vm.

--- init-pre/scale-hdpi/enable.sh
+++ init-pre/scale-hdpi/enable.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 if [ -z "$OS" ]; then
-  . ../lib/common.sh
+  . ../../lib/common.sh
 fi;
 
 if [ "$OS" != "osx" ]; then exit; fi
 
 if [ "$ISHOST" = true ]; then exit; fi
 
+# Copy app
+cp -rf scale-hdpi.app ~/Desktop
+
+# Allow execution
 sudo ~/bin/tccutil.py --insert com.apple.ScriptEditor.id.scale-hdpi
 sudo ~/bin/tccutil.py --enable com.apple.ScriptEditor.id.scale-hdpi
