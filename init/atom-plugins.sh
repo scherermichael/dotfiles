0745f07880ff1b79af7c484758816b39f8e63ceb
Michael Scherer <mail@scherermichael.net>
2016-03-09 16:02:46 +0100

    Update atom-plugins.sh

--- init/atom-plugins.sh
+++ init/atom-plugins.sh
@@ -16,10 +16,8 @@ apm install language-gherkin
 apm install language-powershell
 apm install merge-conflicts
 apm install minimap
-apm install minimap-bookmarks
-apm install minimap-codeglance
-apm install minimap-find-and-replace
 apm install minimap-git-diff
 apm install minimap-selection
+apm install sort-lines
 apm install uuidgen
 apm install white-cursor
