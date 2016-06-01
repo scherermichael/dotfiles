13b278a8f85fcf69a02ff3d6ba1d5718b70501bd
Michael Scherer <mail@scherermichael.net>
2016-06-01 15:49:07 +0200

    Configure git.

---
+++ init/git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+if [ -z "$(which git)" ]; then exit; fi
+
+GIT_USER=${GIT_USER:-Michael Scherer}
+GIT_EMAIL=${GIT_EMAIL:-mail@scherermichael.net}
+
+git config --global user.name "${GIT_USER}"
+git config --global user.email ${GIT_EMAIL}
+git config --global remote.origin.prune true
+git config --global push.default simple
