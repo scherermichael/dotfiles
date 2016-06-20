3f1b3610e0388189ad1d68e3bd8851ab40e00bf6
Michael Scherer <mail@scherermichael.net>
2016-06-20 20:55:16 +0200

    Autosign commits with GPG.

--- init/git.sh
+++ init/git.sh
@@ -7,5 +7,8 @@ GIT_EMAIL=${GIT_EMAIL:-mail@scherermichael.net}
 
 git config --global user.name "${GIT_USER}"
 git config --global user.email ${GIT_EMAIL}
+
 git config --global remote.origin.prune true
 git config --global push.default simple
+
+git config --global commit.gpgsign true
