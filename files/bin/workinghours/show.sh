9606ec7122592e786052b5147d7b230a985bafdf
Michael Scherer <mail@scherermichael.net>
2016-11-09 08:19:33 +0100

    Formatting.

--- files/bin/workinghours/show.sh
+++ files/bin/workinghours/show.sh
@@ -17,13 +17,13 @@ fi
 # Show all entries
 cat "${FILE}"
 
-# Show total time
-TOTAL_MINUTES=$(cat "${FILE}" \
-  | cut -f4 \
-  | cut -d ' ' -f1 \
-  | awk '{sum+=$1}END{print sum}')
-
-FULL_HOURS=$((TOTAL_MINUTES / 60))
-
-echo "---"
-printf "Total time: %d:%02dh\n" ${FULL_HOURS} $((TOTAL_MINUTES - FULL_HOURS * 60))
+# # Show total time
+# TOTAL_MINUTES=$(cat "${FILE}" \
+#   | cut -f4 \
+#   | cut -d ' ' -f1 \
+#   | awk '{sum+=$1}END{print sum}')
+#
+# FULL_HOURS=$((TOTAL_MINUTES / 60))
+#
+# echo "---"
+# printf "Total time: %d:%02dh\n" ${FULL_HOURS} $((TOTAL_MINUTES - FULL_HOURS * 60))
