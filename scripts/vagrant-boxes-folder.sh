f56831ef486228fbe711a414253b50b30889377e
Michael Scherer <michael.scherer@sealsystems.de>
2018-08-16 21:29:08 +0200

    Skip creating global vagrant box folder if it already exists.

--- scripts/vagrant-boxes-folder.sh
+++ scripts/vagrant-boxes-folder.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Skip if folder already exists
+[ -e /opt/vagrant-boxes/ ] && exit
+
 sudo mkdir -p /opt/vagrant-boxes/
 sudo chmod -R +a "staff allow list,add_file,search,add_subdirectory,delete_child,readattr,writeattr,readextattr,writeextattr,readsecurity,file_inherit,directory_inherit" /opt/vagrant-boxes/
 
