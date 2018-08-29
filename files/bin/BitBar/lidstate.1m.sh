235e5d2c5843ab9ca61e788f8b1ccb8988d0780d
Michael Scherer <michael.scherer@sealsystems.de>
2018-08-29 13:40:01 +0200

    Fix lid state checker

---
+++ files/bin/BitBar/lidstate.1m.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# BitBar lid state plugin
+
+isClosed=$(ioreg -r -k AppleClamshellState -d 4 | \
+  grep AppleClamshellState | \
+  head -1)
+result="💻"
+
+if [[ "${isClosed}" =~ "Yes" ]]; then
+  result="💻✓|color=green"
+else
+  if netstat -an | grep .5900 | grep -q ESTABLISHED; then
+    result="✘💻✘|color=red"
+  fi
+fi
+
+echo "${result}"
+echo "---"
+echo "Refresh | refresh=true"
