19e07f55883c53200d641eb3b173aead4d3150db
Michael Scherer <mail@scherermichael.net>
2020-03-30 11:46:14 +0200

    Use One Dark as default theme for Terminal

--- scripts/terminal/README.md
+++ scripts/terminal/README.md
@@ -3,7 +3,7 @@
 Any `.terminal` file in this folder will be added to the Terminal's list of profiles.
 The name of the profile is extracted from the XML contents of the file. Hence, the name of the file is not important.
 
-If you name a file `default.terminal`, it will be set as the default profile.
+Set variable `DEFAULT_PROFILE` in `init.sh` to the profile you want to use as default.
 
 In order to add a new profile or update an existing one, use the export function of the Terminal app. Just save the exported `.terminal` file into this folder.
 
--- scripts/terminal/init.sh
+++ scripts/terminal/init.sh
@@ -10,15 +10,17 @@ set -e
 
 [ "${OS}" = "macos" ] || exit 0
 
-for file in *.terminal; do
+DEFAULT_PROFILE="One Dark"
+
+for file in "${DIR}"/*.terminal; do
   profile=$(sed "/<key>name<\/key>/,/<\/string>/!d" "${file}" | grep "<string>" | sed -E "s|^.*<string>(.*)</string>.*$|\1|")
 
   echo "Importing ${profile}..."
   defaults write com.apple.Terminal "Window Settings" -dict-add "${profile}" "$(sed -e "/<dict>/,/<\/dict>/!d" "${file}" | tr -d '\n')"
-
-  if [ "${file}" == "${DIR}/default.terminal" ]; then
-    echo "Setting $profile as default profile..."
-    defaults write com.apple.terminal "Default Window Settings" -string "${profile}"
-    defaults write com.apple.terminal "Startup Window Settings" -string "${profile}"
-  fi
 done
+
+if [ -n "${DEFAULT_PROFILE}" ]; then
+  echo "Setting ${DEFAULT_PROFILE} as default profile..."
+  defaults write com.apple.terminal "Default Window Settings" -string "${DEFAULT_PROFILE}"
+  defaults write com.apple.terminal "Startup Window Settings" -string "${DEFAULT_PROFILE}"
+fi
