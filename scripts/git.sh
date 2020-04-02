cbc2be0fe452b2b60aee9f994f05480a00820629
Michael Scherer <mail@scherermichael.net>
2020-04-02 15:35:51 +0200

    Update Git settings

--- scripts/git.sh
+++ scripts/git.sh
@@ -6,9 +6,15 @@ GIT_USER=${GIT_USER:-Michael Scherer}
 GIT_EMAIL=${GIT_EMAIL:-mail@scherermichael.net}
 
 git config --global user.name "${GIT_USER}"
-git config --global user.email ${GIT_EMAIL}
+git config --global user.email "${GIT_EMAIL}"
 
+# Global exclude file (see also: files/.gitexcludes)
+git config --global core.excludesfile "${HOME}/.gitexcludes"
+# Remove obsolete remote branches
 git config --global remote.origin.prune true
+# Push only to a remote branch with the same name
 git config --global push.default simple
-
+# User GPG to sign commits
 git config --global commit.gpgsign true
+# Simply print list of branches
+git config --global pager.branch false
