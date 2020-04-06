3a27c412592ea5f19f0934912f1953c6304c8f4b
Michael Scherer <mail@scherermichael.net>
2020-04-06 11:28:12 +0200

    Allow to rollback changes to pam config

--- files/bin/enable-sudo-touchid-watch.sh
+++ files/bin/enable-sudo-touchid-watch.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+set -e
+
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [[ $UID -ne 0 ]]; then
+   echo "This script must be run as root."
+   exit 1
+fi
+
 if grep "pam_watchid.so" -q /etc/pam.d/sudo; then
   echo "PAM modules are already installed."
   exit 0
@@ -10,15 +20,32 @@ echo "Moving pam_watchid.so to destination path..."
 VERSION=2
 LIBRARY_NAME=pam_watchid.so
 DESTINATION=/usr/local/lib/pam
-sudo mkdir -p ${DESTINATION}
-sudo cp ${LIBRARY_NAME} ${DESTINATION}/${LIBRARY_NAME}.${VERSION}
-sudo chmod 444 ${DESTINATION}/${LIBRARY_NAME}.${VERSION}
-sudo chown root:wheel ${DESTINATION}/${LIBRARY_NAME}.${VERSION}
+mkdir -p ${DESTINATION}
+cp "${dir}/${LIBRARY_NAME}" ${DESTINATION}/${LIBRARY_NAME}.${VERSION}
+chmod 444 ${DESTINATION}/${LIBRARY_NAME}.${VERSION}
+chown root:wheel ${DESTINATION}/${LIBRARY_NAME}.${VERSION}
 
 echo "Patching /etc/pam.d/sudo ..."
-sudo sed -i '' '1a\
+cp /etc/pam.d/sudo /etc/pam.d/sudo.bak
+sed -i '' '1a\
 auth       sufficient     pam_watchid.so "reason=execute a command as root"\
 auth       sufficient     pam_tid.so
 ' /etc/pam.d/sudo
 
-echo "PAM modules installed. Please open a new shell."
+echo "PAM modules installed:"
+echo "------------------------------------------------------"
+cat /etc/pam.d/sudo
+echo "------------------------------------------------------"
+echo ""
+echo "Please open a new shell and try the new mechanism e.g. with: sudo whoami"
+echo "After verifying that the modification works, type 'y' to keep it."
+echo "Any other key will restore the original config."
+echo "Press any key..."
+
+read -rsn1 input
+if [ "$input" != "y" ]; then
+  echo "Rolling back changes..."
+  mv /etc/pam.d/sudo.bak /etc/pam.d/sudo
+fi
+
+echo "Done."
