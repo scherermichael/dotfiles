ca1d5f7abad8e5b23f90b69faa0c8f5751651fe3
Michael Scherer <mail@scherermichael.net>
2020-05-06 23:39:22 +0200

    Remove unneeded variable

--- scripts/terminal-ubuntu/init.sh
+++ scripts/terminal-ubuntu/init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
 set -e
 
 [ -z "${OS}" ] && . lib/common.sh
