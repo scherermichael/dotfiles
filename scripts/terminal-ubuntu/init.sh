a271c9e19af0e050f608bb2b95857e57435f623d
Michael Scherer <mail@scherermichael.net>
2020-05-06 23:12:17 +0200

    Linux: Configure termial

---
+++ scripts/terminal-ubuntu/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+set -e
+
+[ -z "${OS}" ] && . lib/common.sh
+
+[ "${OS}" = "linux" ] || exit 0
+
+echo "Installing Atom One Dark color scheme..."
+curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh
+
+echo "Switch to zsh as default shell..."
+chsh -s $(which zsh)
