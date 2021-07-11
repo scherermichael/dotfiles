7e44443f3d324120bd60774641143acd0553627f
Michael Scherer <mail@scherermichael.net>
2021-07-11 12:50:10 +0200

    .

--- files/bin/connect-peripherals.sh
+++ files/bin/connect-peripherals.sh
@@ -3,6 +3,7 @@
 for id in ${BT_AUTOCONNECT_IDS}; do
   echo "Connecting $id..."
   blueutil --info "${id}"
+  blueutil --disconnect "${id}"
   blueutil --pair "${id}"
   blueutil --connect "${id}"
   echo "Done"
