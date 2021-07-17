58001e7b61f5b9af3fe35dafbd835daa54504204
Michael Scherer <mail@scherermichael.net>
2021-07-17 08:54:13 +0200

    Retry to connect bluetooth devices

--- files/bin/connect-peripherals.sh
+++ files/bin/connect-peripherals.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+if [ -z "${BT_AUTOCONNECT_IDS}" ]; then
+  echo "Error: No ids of devices to connect found."
+  exit 1
+fi
 
 for id in ${BT_AUTOCONNECT_IDS}; do
-  echo "Connecting $id..."
-  blueutil --info "${id}"
-  blueutil --disconnect "${id}" >/dev/null 2>&1
-  blueutil --pair "${id}" 0000
-  blueutil --connect "${id}"
-  echo "Done"
+  echo "- Connecting $id..."
+  while true; do
+    blueutil --info "${id}"
+    blueutil --disconnect "${id}" >/dev/null 2>&1
+    blueutil --pair "${id}" 0000
+    blueutil --connect "${id}"
+
+    if [ "$(blueutil --is-connected "${id}")" = "1" ]; then
+      break
+    fi
+
+    echo "- Attempt failed. Retrying..."
+  done
+  echo "- Done"
 done
+
+echo "All devices connected."
