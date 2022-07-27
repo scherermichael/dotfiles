918a883dbe93e242da8839e065a7f873553eb27b
Michael Scherer <mail@scherermichael.net>
2022-07-27 22:59:39 +0200

    Install Rosetta 2 on ARM

---
+++ scripts/00_rosetta.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+[ -z "${OS}" ] && . lib/common.sh
+
+[ "${OS}" = "macos" ] || exit 0
+
+if [ "$(uname -p)" = "arm" ]; then
+  echo "Installing Rosetta 2..."
+  softwareupdate --install-rosetta --agree-to-license
+fi
