7def39c7059aa8d4903e48572e06b84b3a494edc
Michael Scherer <mail@scherermichael.net>
2022-07-27 23:00:08 +0200

    Fix reinstall of ohmyz.sh

--- scripts/ohmyz.sh
+++ scripts/ohmyz.sh
@@ -1,3 +1,9 @@
-# Unset the ZSH variable when calling the installer
+#!/bin/bash
+
+echo Unsetting the ZSH variable...
 export ZSH=
+echo Creating backup of install folder...
+rm -rf ~/.oh-my-zsh.bak
+mv ~/.oh-my-zsh ~/.oh-my-zsh.bak
+echo Installing zsh...
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
