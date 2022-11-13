40ac69a7f9433f2965f3ecf6430eb0807c96c2be
Michael Scherer <mail@scherermichael.net>
2022-11-13 13:54:40 +0100

    Explicitly use 'source'

--- files/bin/kops-login.sh
+++ files/bin/kops-login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "You must source this script in order to use the new environment in your shell!"
-echo "Example: . $0"
+echo "Example: source $0"
 
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_KOPS}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_KOPS}
