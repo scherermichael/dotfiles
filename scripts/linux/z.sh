62d09174018392cdecc3ec01994edf04c05f2388
Michael Scherer <mail@scherermichael.net>
2022-11-13 18:02:46 +0100

    .

--- scripts/linux/z.sh
+++ scripts/linux/z.sh
@@ -9,9 +9,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 [ "${OS}" = "linux" ] || exit 0
 
-# Download to latest to home dir
+# Download latest version to home dir
 wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/z.sh
-# Add to .bashrc
-echo source /path/to/z.sh >> ~/.bashrc
-# Add to .zshrc
-echo source /path/to/z.sh >> ~/.zshrc
