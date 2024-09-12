92312a28a7d7ec017b6596ce3c1af3406173ae86
Michael Scherer <mail@scherermichael.net>
2024-09-12 22:54:45 +0200

    .

---
+++ files/start-default-apps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+aerospace workspace W
+sleep 1
+open "/Applications/Safari.app"
+sleep 1
+aerospace workspace B
+sleep 1
+open "${HOME}/Applications/Obsidian.app"
+sleep 1
+aerospace workspace C
+sleep 1
+open "/System/Applications/Mail.app"
