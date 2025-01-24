2105d3164995a3b93a6df38cf0e662238415403f
Michael Scherer <mail@scherermichael.net>
2025-01-25 00:23:26 +0100

    pull first

--- files/bin/scribe2obsidian-commit.sh
+++ files/bin/scribe2obsidian-commit.sh
@@ -1,5 +1,6 @@
 osascript -e "display notification \"Committing ${title} to vault.\" with title \"PDF from Scribe to Obsidian\" subtitle \"${vaultName}\""
 cd "${vaultPath}" || exit 202
-git add "${title}.pdf" "${title}.md" || exit 203
-git commit -m "Add note ${title}" || exit 204
-git push || exit 205
+git pull || exit 203
+git add "${title}.pdf" "${title}.md" || exit 204
+git commit -m "Add note ${title}" || exit 205
+git push || exit 206
